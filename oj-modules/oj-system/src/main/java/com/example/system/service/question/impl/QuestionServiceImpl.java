0529d5ed8d3566ea54f7e61937038951218e0017
dank <1923205436@qq.com>
2025-09-01 03:54:30 +0800

    feat: 题目信息新增|详情、编辑|删除功能

--- oj-modules/oj-system/src/main/java/com/example/system/service/question/impl/QuestionServiceImpl.java
+++ oj-modules/oj-system/src/main/java/com/example/system/service/question/impl/QuestionServiceImpl.java
@@ -1,6 +1,11 @@
 package com.example.system.service.question.impl;
 
 
+import cn.hutool.core.collection.CollectionUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.example.commom.core.enums.ResultCode;
+import com.example.common.security.exception.ServiceException;
+import com.example.system.domain.question.Question;
 import com.example.system.domain.question.dto.QuestionAddDTO;
 import com.example.system.domain.question.dto.QuestionEditDTO;
 import com.example.system.domain.question.dto.QuestionQueryDTO;
@@ -10,8 +15,10 @@ import com.example.system.mapper.question.QuestionMapper;
 import com.example.system.service.question.IQuestionService;
 import com.github.pagehelper.PageHelper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -29,30 +36,122 @@ public class QuestionServiceImpl implements IQuestionService {
 //    @Autowired
 //    private QuestionCacheManager questionCacheManager;
 
+    /**
+     * 查询问题列表
+     *
+     * @param questionQueryDTO 问题查询条件DTO，包含分页信息和查询条件
+     * @return 返回问题列表，包含分页数据
+     */
     @Override
     public List<QuestionVO> list(QuestionQueryDTO questionQueryDTO) {
+        // 使用PageHelper进行分页处理，设置当前页码和每页大小
         PageHelper.startPage(questionQueryDTO.getPageNum(), questionQueryDTO.getPageSize());
+        // 调用数据访问层方法查询问题列表
         List<QuestionVO> questionVOList = questionMapper.selectQuestionList(questionQueryDTO);
         return questionVOList;
     }
 
+    /**
+     * 添加问题方法
+     *
+     * @param questionAddDTO 添加问题的数据传输对象
+     * @return 添加成功返回true，否则返回false
+     */
     @Override
     public boolean add(QuestionAddDTO questionAddDTO) {
-        return false;
+        // 查询数据库中是否已存在相同标题的问题
+        List<Question> questionList = questionMapper.selectList(new LambdaQueryWrapper<Question>()
+                .eq(Question::getTitle, questionAddDTO.getTitle()));
+        // 如果问题列表不为空，说明已存在相同标题的问题，抛出异常
+        if (CollectionUtil.isNotEmpty(questionList)) {
+            throw new ServiceException(ResultCode.FAILED_ALREADY_EXISTS);
+        }
+        // 创建新的问题对象，并将DTO的属性复制到问题对象中
+        Question question = new Question();
+        BeanUtils.copyProperties(questionAddDTO, question);
+        // 将问题插入数据库
+        int insert = questionMapper.insert(question);
+        // 判断插入是否成功
+        if (insert <= 0) {
+            return false;
+        }
+        return true;
     }
 
+
+    /**
+     * 获取问题详情的方法
+     *
+     * @param questionId 问题ID
+     * @return QuestionDetailVO 问题详情的视图对象
+     */
     @Override
     public QuestionDetailVO detail(Long questionId) {
-        return null;
+        // 根据问题ID查询问题信息
+        Question question = questionMapper.selectById(questionId);
+        // 判断问题是否存在，不存在则抛出异常
+        if (question == null) {
+            throw new ServiceException(ResultCode.FAILED_NOT_EXISTS);
+        }
+        // 创建问题详情视图对象
+        QuestionDetailVO questionDetailVO = new QuestionDetailVO();
+        // 将问题对象的属性复制到视图对象中
+        BeanUtils.copyProperties(question, questionDetailVO);
+        // 返回问题详情视图对象
+        return questionDetailVO;
     }
 
+    /**
+     * 编辑问题信息的方法
+     *
+     * @param questionEditDTO 包含问题编辑信息的DTO对象
+     * @return 返回更新的记录数
+     * @throws ServiceException 当要编辑的问题不存在时抛出异常
+     */
     @Override
     public int edit(QuestionEditDTO questionEditDTO) {
-        return 0;
+        // 根据问题ID查询旧的问题信息
+        Question oldQuestion = questionMapper.selectById(questionEditDTO.getQuestionId());
+        // 检查问题是否存在，不存在则抛出异常
+        if (oldQuestion == null) {
+            throw new ServiceException(ResultCode.FAILED_NOT_EXISTS);
+        }
+        // 更新问题的标题
+        oldQuestion.setTitle(questionEditDTO.getTitle());
+        // 更新问题的难度
+        oldQuestion.setDifficulty(questionEditDTO.getDifficulty());
+        // 更新问题的时间限制
+        oldQuestion.setTimeLimit(questionEditDTO.getTimeLimit());
+        // 更新问题的空间限制
+        oldQuestion.setSpaceLimit(questionEditDTO.getSpaceLimit());
+        // 更新问题的内容
+        oldQuestion.setContent(questionEditDTO.getContent());
+        // 更新问题的测试用例
+        oldQuestion.setQuestionCase(questionEditDTO.getQuestionCase());
+        // 更新问题的默认代码
+        oldQuestion.setDefaultCode(questionEditDTO.getDefaultCode());
+        // 更新问题的主函数
+        oldQuestion.setMainFuc(questionEditDTO.getMainFuc());
+        // 执行更新操作并返回更新的记录数
+        return questionMapper.updateById(oldQuestion);
     }
 
+
     @Override
+    /**
+     * 根据问题ID删除问题
+     * @param questionId 问题ID
+     * @return 返回受影响的行数，如果删除成功则返回1
+     * @throws ServiceException 当问题不存在时抛出异常
+     */
     public int delete(Long questionId) {
-        return 0;
+        // First, check if the question exists
+        Question question = questionMapper.selectById(questionId);
+        if (question == null) {
+            // Throw exception if question doesn't exist
+            throw new ServiceException(ResultCode.FAILED_NOT_EXISTS);
+        }
+        // If exists, delete the question and return affected rows
+        return questionMapper.deleteById(questionId);
     }
 }
