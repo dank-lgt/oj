513f8f15099bcbf8d06d07bef331df40490ef313
dank <1923205436@qq.com>
2026-03-01 15:53:05 +0800

    feat: 更新项目路径配置并升级MySQL版本

--- deploy/test/sh/copy.ps1
+++ deploy/test/sh/copy.ps1
@@ -1,12 +1,12 @@
-rm E:/examplepro/example-oj/deploy/test/bitoj-jar/gateway/oj-gateway.jar
-rm E:/examplepro/example-oj/deploy/test/bitoj-jar/friend/oj-friend.jar
-rm E:/examplepro/example-oj/deploy/test/bitoj-jar/job/oj-job.jar
-rm E:/examplepro/example-oj/deploy/test/bitoj-jar/judge/oj-judge.jar
-rm E:/examplepro/example-oj/deploy/test/bitoj-jar/system/oj-system.jar
+rm E:\oj\Web-oj/deploy/test/bitoj-jar/gateway/oj-gateway.jar
+rm E:\oj\Web-oj/deploy/test/bitoj-jar/friend/oj-friend.jar
+rm E:\oj\Web-oj/deploy/test/bitoj-jar/job/oj-job.jar
+rm E:\oj\Web-oj/deploy/test/bitoj-jar/judge/oj-judge.jar
+rm E:\oj\Web-oj\deploy/test/bitoj-jar/system/oj-system.jar
 
-copy E:/examplepro/example-oj/oj-gateway/target/oj-gateway-1.0-SNAPSHOT.jar E:/examplepro/example-oj/deploy/test/bitoj-jar/gateway/oj-gateway.jar
-copy E:/examplepro/example-oj/oj-modules/oj-judge/target/oj-judge-1.0-SNAPSHOT.jar E:/examplepro/example-oj/deploy/test/bitoj-jar/judge/oj-judge.jar
-copy E:/examplepro/example-oj/oj-modules/oj-friend/target/oj-friend-1.0-SNAPSHOT.jar E:/examplepro/example-oj/deploy/test/bitoj-jar/friend/oj-friend.jar
-copy E:/examplepro/example-oj/oj-modules/oj-job/target/oj-job-1.0-SNAPSHOT.jar E:/examplepro/example-oj/deploy/test/bitoj-jar/job/oj-job.jar
-copy E:/examplepro/example-oj/oj-modules/oj-system/target/oj-system-1.0-SNAPSHOT.jar E:/examplepro/example-oj/deploy/test/bitoj-jar/system/oj-system.jar
+copy E:\oj\Web-oj/oj-gateway/target/oj-gateway-1.0-SNAPSHOT.jar E:/examplepro/example-oj/deploy/test/bitoj-jar/gateway/oj-gateway.jar
+copy E:\oj\Web-oj/oj-modules/oj-judge/target/oj-judge-1.0-SNAPSHOT.jar E:/examplepro/example-oj/deploy/test/bitoj-jar/judge/oj-judge.jar
+copy E:\oj\Web-oj/oj-modules/oj-friend/target/oj-friend-1.0-SNAPSHOT.jar E:/examplepro/example-oj/deploy/test/bitoj-jar/friend/oj-friend.jar
+copy E:\oj\Web-oj/oj-modules/oj-job/target/oj-job-1.0-SNAPSHOT.jar E:/examplepro/example-oj/deploy/test/bitoj-jar/job/oj-job.jar
+copy E:\oj\Web-oj/oj-modules/oj-system/target/oj-system-1.0-SNAPSHOT.jar E:/examplepro/example-oj/deploy/test/bitoj-jar/system/oj-system.jar
 pause
--- oj-modules/oj-friend/src/main/java/com/example/friend/test/NginxTestController.java
+++ oj-modules/oj-friend/src/main/java/com/example/friend/test/NginxTestController.java
@@ -1,29 +1,30 @@
-//package com.example.friend.test;
-//
-//
-//import com.example.commom.core.controller.BaseController;
-//import com.example.commom.core.domain.R;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/test/nginx")
-//@Slf4j
-//public class NginxTestController  extends BaseController {
-//
-//    @GetMapping("/info")
-//    public R<Void> info() {
-//        log.info("负载均衡测试");
-//        return R.ok();
-//    }
+package com.example.friend.test;
 
-//    location /bitoj-dev/ {
-//        proxy_pass http://bitoj/;
+
+import com.example.commom.core.controller.BaseController;
+import com.example.commom.core.domain.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test/nginx")
+@Slf4j
+public class NginxTestController  extends BaseController {
+
+    @GetMapping("/info")
+    public R<Void> info() {
+        log.info("负载均衡测试");
+        return R.ok();
+    }
+
+
+//    location /Web-oj/ {
+//        proxy_pass http://Weboj/;
 //    }
 
 //    upstream bitoj {
-//        server 192.168.100.136:9202 weight=1;
-//        server 192.168.100.136:19202 weight=2;
-//    }
+//        server 192.168.16.1:9202 weight=1;
+//        server 192.168.16.1:19202 weight=2;
+    }
